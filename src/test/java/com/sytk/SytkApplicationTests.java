0212c712ce6b6f6b49ac3c99db5bbc693a497821
ghsyn <swanshawn715@gmail.com>
2026-02-05 21:58:40 +0900

    Init: Root Project 생성

---
+++ src/test/java/com/sytk/SytkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sytk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SytkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
