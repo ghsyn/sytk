1385f0608d8ba203bf6a3d41c96b27915d301211
ghsyn <swanshawn715@gmail.com>
2026-03-11 20:41:36 +0900

    Feat: read 모듈 내 공연 entity 및 repository 추가
    - CQRS 패턴을 위해 조회 관련 로직 read 모듈로 이관

---
+++ sytk-read/src/main/java/com/sytk/read/domain/Concert.java
@@ -0,0 +1,51 @@
+package com.sytk.read.domain;
+
+import com.sytk.booking.domain.SeatGrade;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class Concert {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private OffsetDateTime startAt;
+
+    private Integer runningTime;
+
+    @Column(nullable = false)
+    private String location;
+
+    private OffsetDateTime ticketOpenAt;
+
+    private OffsetDateTime ticketCloseAt;
+
+    @OneToMany(mappedBy = "concert")
+    private List<SeatGrade> seatGrades = new ArrayList<>();
+
+    @Builder
+    public Concert(String title, OffsetDateTime startAt, Integer runningTime, String location,
+                   OffsetDateTime ticketOpenAt, OffsetDateTime ticketCloseAt) {
+        this.title = title;
+        this.startAt = startAt;
+        this.runningTime = runningTime;
+        this.location = location;
+        this.ticketOpenAt = ticketOpenAt;
+        this.ticketCloseAt = ticketCloseAt;
+    }
+}
