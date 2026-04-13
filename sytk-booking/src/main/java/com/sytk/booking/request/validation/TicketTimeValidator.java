55678e01031d4f170742ecff30bdaec2199672e0
ghsyn <swanshawn715@gmail.com>
2026-04-14 01:20:39 +0900

    Feat: TicketTimeValidator 추가

---
+++ sytk-booking/src/main/java/com/sytk/booking/request/validation/TicketTimeValidator.java
@@ -0,0 +1,16 @@
+package com.sytk.booking.request.validation;
+
+import com.sytk.booking.request.ConcertEditRequest;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class TicketTimeValidator implements ConstraintValidator<ValidTicketTime, ConcertEditRequest> {
+    @Override
+    public boolean isValid(ConcertEditRequest value, ConstraintValidatorContext context) {
+        if (value.ticketOpenAt() == null || value.ticketCloseAt() == null) {
+            return true;
+        }
+
+        return value.ticketCloseAt().isBefore(value.ticketCloseAt());
+    }
+}
